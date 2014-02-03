a5e502d5fa0fd97276333c10e8130fcff3b03e66
David Yen <dyen@chromium.org>
2014-02-03 11:08:39 -0800

    Added install scripts for mac/linux

---
+++ Scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -n "$1" ]; then
+  echo Usage: install.sh SUBLIME_CONFIG_DIR
+  exit 1
+fi
+
+SUBLIME_DIR=$1
+if [ ! -d $SUBLIME_DIR/Packages ]; then
+  echo Install script expects sublime directory as the first argument
+  exit 1 
+fi
+
+USER_PREFS=$SUBLIME_DIR/Packages/User
+USER_PREFS_BAK=$USER_PREFS.bak
+SUBLIME_PREFS=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../UserPrefs")
+echo Backing up User Preferences to "$USER_PREFS"
+mv "$USER_PREFS" "$USER_PREFS_BAK"
+
+echo Linking Sublime User Preferences...
+ln -s "$SUBLIME_PREFS" "$USER_PREFS"
+
+exit
