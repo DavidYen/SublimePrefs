43a8cc701bcc1a41740c5279890b90ec04f39210
David Yen <dyen@google.com>
2014-02-03 12:24:45 -0800

    Fixed install script so it works on mac as well

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,14 +5,15 @@ if [ ! -n "$1" ]; then
 fi
 
 SUBLIME_DIR=$1
-if [ ! -d $SUBLIME_DIR/Packages ]; then
+if [ ! -d "$SUBLIME_DIR/Packages" ]; then
   echo Install script expects sublime directory as the first argument
   exit 1 
 fi
 
 USER_PREFS=$SUBLIME_DIR/Packages/User
 USER_PREFS_BAK=$USER_PREFS.bak
-SUBLIME_PREFS=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/../UserPrefs")
+SUBLIME_PREFS=$(pwd)/$(dirname "${BASH_SOURCE[0]}")/../UserPrefs
+echo $SUBLIME_PREFS
 echo Backing up User Preferences to "$USER_PREFS"
 mv "$USER_PREFS" "$USER_PREFS_BAK"
 
